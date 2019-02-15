c3dbf6a54c8cd022cc01b9f11d84e2f446e967ab
fjasper13 <fulgentiusjasper@gmail.com>
2019-02-15 13:34:39 +0700

    update coding challange and spinner

--- app/src/main/java/com/example/android/DroidCafeInput/OrderActivity.java
+++ app/src/main/java/com/example/android/DroidCafeInput/OrderActivity.java
@@ -20,15 +20,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.RadioButton;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.AdapterView;
+
 
 /**
  * This activity shows the order chosen.  The order is sent as data
  * with the intent to launch this activity.
  */
-public class OrderActivity extends AppCompatActivity {
+public class OrderActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +46,22 @@ public class OrderActivity extends AppCompatActivity {
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
         TextView textView = findViewById(R.id.order_textview);
         textView.setText(message);
+
+        RadioButton buttonsame=findViewById(R.id.sameday);
+        buttonsame.setChecked(true);
+
+        Spinner spinner=findViewById(R.id.label_spinner);
+        if(spinner!=null){
+            spinner.setOnItemSelectedListener(this);
+        }
+        ArrayAdapter<CharSequence> adapter=ArrayAdapter.createFromResource(this,
+                R.array.labels_array,android.R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource
+                (android.R.layout.simple_spinner_dropdown_item);
+        if (spinner != null) {
+            spinner.setAdapter(adapter);
+        }
     }
 
     public void displayToast(String message){
@@ -64,7 +86,19 @@ public class OrderActivity extends AppCompatActivity {
                     displayToast(getString(R.string.pick_up));
                 break;
             default:
+                //Do nothing
                 break;
         }
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        String spinnerLabel = adapterView.getItemAtPosition(i).toString();
+        displayToast(spinnerLabel);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
